08533e79cdc35c540e8069347958450bd479a721
Gina Marieges <gina.marieges@gmail.com>
2023-08-06 16:38:24 +0200

    Add Router Provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import "@fontsource/montserrat";
-import App from "./components/App/App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import GlobalStyle from "./styles/GlobalStyle/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme/theme";
+import appRouter from "./routers/appRouter";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <GlobalStyle />
-        <App />
+        <RouterProvider router={appRouter} />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
