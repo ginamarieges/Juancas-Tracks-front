3f2f93de04c7e979ac6bacee4104f8792a14e8cd
Gina Marieges <gina.marieges@gmail.com>
2023-08-24 19:08:16 +0200

    Add LoginPage in appRouter with new path

--- src/routers/appRouter.tsx
+++ src/routers/appRouter.tsx
@@ -1,12 +1,17 @@
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
 import RegisterPage from "../components/pages/ReqisterPage/RegisterPage";
+import { paths } from "./paths/paths";
+import LoginPage from "../components/pages/LoginPage/LoginPage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/register", element: <RegisterPage /> }],
+    children: [
+      { path: paths.register, element: <RegisterPage /> },
+      { path: paths.login, element: <LoginPage /> },
+    ],
   },
 ];
 
--- src/routers/paths/types.ts
+++ src/routers/paths/types.ts
@@ -1,5 +1,6 @@
 interface PathsStructure {
   register: string;
+  login: string;
 }
 
 export default PathsStructure;
