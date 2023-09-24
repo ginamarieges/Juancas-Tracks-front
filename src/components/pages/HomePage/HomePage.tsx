6617d4600abec4570cdbe3f2343d97e1a3919fe6
Gina Marieges <gina.marieges@gmail.com>
2023-09-24 16:06:17 +0200

    Add list in HomePage

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -3,10 +3,12 @@ import HomePageStyled from "./HomePageStyled";
 import useTracks from "../../../hooks/useTracks/useTracks";
 import { useDispatch } from "react-redux";
 import { loadTracksActionCreator } from "../../../store/tracks/tracksSlice";
+import { useAppSelector } from "../../../store";
 
 const HomePage = (): React.ReactElement => {
   const { getTracks } = useTracks();
   const dispatch = useDispatch();
+  const { tracks } = useAppSelector((state) => state.tracksStore);
 
   useEffect(() => {
     (async () => {
@@ -20,6 +22,11 @@ const HomePage = (): React.ReactElement => {
   return (
     <HomePageStyled>
       <h1 className="title">Todos tus vinilos</h1>
+      <ul className="list">
+        {tracks.map((tracks) => (
+          <li key={tracks.id}>name: {tracks.album} </li>
+        ))}
+      </ul>
     </HomePageStyled>
   );
 };
