fdc7e4a66f3399a3b49a991b8ca70c20f9d652b9
Gina Marieges <gina.marieges@gmail.com>
2023-09-24 15:38:06 +0200

    Add getTracks happy path test

--- src/hooks/useTracks/useTracks.tsx
+++ src/hooks/useTracks/useTracks.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { TrackStructure, TracksStateStructure } from "../../store/tracks/types";
 
 const useTracks = () => {
   const apiUrl = import.meta.env.VITE_API_URL;
 
-  const getTracks = async () => {
+  const getTracks = async (): Promise<TracksStateStructure> => {
     const {
       data: { tracks },
-    } = await axios.get(`${apiUrl}/tracks`);
+    } = await axios.get<{ tracks: TrackStructure[] }>(`${apiUrl}/tracks`);
 
     return { tracks };
   };
