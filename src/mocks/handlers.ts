5edc3221b9c13ef98005a8be570f21b369bde8f2
Gina Marieges <gina.marieges@gmail.com>
2023-09-24 15:22:13 +0200

    Create handler of getTracks

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { rest } from "msw";
 import { registerUserMock, tokenMock } from "./userMocks";
+import { tracksMock } from "./tracksMocks";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -10,6 +11,10 @@ export const handlers = [
 
   rest.post(`${apiUrl}/user/login`, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ token: tokenMock }));
+  }),
+
+  rest.get(`${apiUrl}/tracks`, (_req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ tracks: tracksMock }));
   }),
 ];
 
