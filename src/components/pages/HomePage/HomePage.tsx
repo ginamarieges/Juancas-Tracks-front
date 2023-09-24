869726c10f07c06472d54c349a7323c5b58f2d7b
Gina Marieges <gina.marieges@gmail.com>
2023-09-24 15:59:30 +0200

    Add useEffect to dispatch loadTracks

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import HomePageStyled from "./HomePageStyled";
+import useTracks from "../../../hooks/useTracks/useTracks";
+import { useDispatch } from "react-redux";
+import { loadTracksActionCreator } from "../../../store/tracks/tracksSlice";
 
 const HomePage = (): React.ReactElement => {
+  const { getTracks } = useTracks();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const tracks = await getTracks();
+      if (tracks) {
+        dispatch(loadTracksActionCreator(tracks));
+      }
+    })();
+  }, [dispatch, getTracks]);
+
   return (
     <HomePageStyled>
       <h1 className="title">Todos tus vinilos</h1>
--- src/hooks/useTracks/useTracks.tsx
+++ src/hooks/useTracks/useTracks.tsx
@@ -1,16 +1,17 @@
 import axios from "axios";
 import { TrackStructure, TracksStateStructure } from "../../store/tracks/types";
+import { useCallback } from "react";
 
 const useTracks = () => {
   const apiUrl = import.meta.env.VITE_API_URL;
 
-  const getTracks = async (): Promise<TracksStateStructure> => {
+  const getTracks = useCallback(async (): Promise<TracksStateStructure> => {
     const {
       data: { tracks },
     } = await axios.get<{ tracks: TrackStructure[] }>(`${apiUrl}/tracks`);
 
     return { tracks };
-  };
+  }, [apiUrl]);
 
   return { getTracks };
 };
