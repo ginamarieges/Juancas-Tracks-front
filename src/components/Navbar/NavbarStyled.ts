27f05fd24062d757a9e674c214fb5933fa6fa345
Gina Marieges <gina.marieges@gmail.com>
2023-09-30 20:02:04 +0200

    Add media queries in navbar

--- src/components/Navbar/NavbarStyled.ts
+++ src/components/Navbar/NavbarStyled.ts
@@ -8,12 +8,45 @@ const NavbarStyled = styled.div`
   padding: 10px 20px;
   z-index: 1;
 
+  @media (min-width: 600px) {
+    position: static;
+    order: -1;
+
+    .home-mobile,
+    .add-mobile,
+    .logout-mobile {
+      display: none;
+    }
+  }
+
+  .home-desktop,
+  .add-desktop,
+  .logout-desktop {
+    display: none;
+
+    @media (min-width: 600px) {
+      display: contents;
+      color: ${(props) => props.theme.color.text};
+
+      &:hover {
+        color: ${(props) => props.theme.color.secondary};
+      }
+    }
+  }
+
   .navbar {
     min-width: 280px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
+
+    @media (min-width: 600px) {
+      padding-top: 20px;
+      justify-content: flex-end;
+      gap: 50px;
+      text-transform: uppercase;
+    }
   }
 `;
 
