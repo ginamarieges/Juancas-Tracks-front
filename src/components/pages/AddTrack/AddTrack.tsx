90fc33fbaa5f1d2274ecb1364bfdeabeb4532f8f
Gina Marieges <gina.marieges@gmail.com>
2023-10-01 17:47:11 +0200

    Create AddTrack page

---
+++ src/components/pages/AddTrack/AddTrack.tsx
@@ -0,0 +1,12 @@
+import Form from "../../Form/Form";
+
+const AddTrack = (): React.ReactElement => {
+  return (
+    <>
+      <h1>Añade tu vinilo</h1>
+      <Form />
+    </>
+  );
+};
+
+export default AddTrack;
