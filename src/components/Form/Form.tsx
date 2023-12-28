7801fb33af65a3ce9d1609e2ae37f70d241c19dc
Gina Marieges <gina.marieges@gmail.com>
2023-12-28 20:10:40 +0100

    Modify add image for add url

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,21 +1,72 @@
+import { useState } from "react";
 import FormStyled from "./FormStyled";
+import { TrackStructure } from "../../store/tracks/types";
 
 const Form = (): React.ReactElement => {
+  const initialTrackData: TrackStructure = {
+    album: "",
+    id: "",
+    image: "",
+    musicType: "",
+    notes: "",
+    repeat: false,
+    singer: "",
+    songs: [],
+    user: "",
+  };
+
+  const [trackData, setTrackData] = useState(initialTrackData);
+
+  const onChangeData = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setTrackData({
+      ...trackData,
+      [event.target.id]: event.target.value,
+    });
+  };
+  const onChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTrackData({
+      ...trackData,
+      [event.target.id]: event.target.checked,
+    });
+  };
+
+  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setTrackData(initialTrackData);
+  };
   return (
     <FormStyled
       className="form"
-      action="/upload_files"
       encType="multipart/form-data"
       autoComplete="off"
       noValidate
+      onSubmit={handleOnSubmit}
     >
       <div className="form__controls">
         <label htmlFor="album">Álbum</label>
-        <input className="form__input" type="text" name="album" id="album" />
+        <input
+          className="form__input"
+          type="text"
+          name="album"
+          id="album"
+          value={trackData.album}
+          onChange={onChangeData}
+        />
       </div>
       <div className="form__controls">
         <label htmlFor="singer">Cantante</label>
-        <input className="form__input" type="text" name="singer" id="singer" />
+        <input
+          className="form__input"
+          type="text"
+          name="singer"
+          id="singer"
+          value={trackData.singer}
+          onChange={onChangeData}
+        />
       </div>
       <div className="checkbox">
         <label htmlFor="repeat">¿Repetido?</label>
@@ -24,6 +75,7 @@ const Form = (): React.ReactElement => {
           type="checkbox"
           name="repeat"
           id="repeat"
+          onChange={onChangeCheckbox}
         />
       </div>
       <div className="form__controls">
@@ -33,6 +85,8 @@ const Form = (): React.ReactElement => {
           type="text"
           name="musicType"
           id="musicType"
+          value={trackData.musicType}
+          onChange={onChangeData}
         />
       </div>
       <div className="form__controls">
@@ -43,11 +97,19 @@ const Form = (): React.ReactElement => {
           id="notes"
           cols={30}
           rows={7}
+          value={trackData.notes}
+          onChange={onChangeData}
         ></textarea>
       </div>
       <div className="form__controls">
-        <label htmlFor="image">Selecciona una imagen</label>
-        <input type="file" name="image" id="image" accept="image/*" />
+        <label htmlFor="image">Portada</label>
+        <input
+          className="form__input"
+          type="text"
+          id="image"
+          value={trackData.image}
+          onChange={onChangeData}
+        />
       </div>
       <div className="form__controls">
         <label htmlFor="songs">Canciones</label>
@@ -57,6 +119,8 @@ const Form = (): React.ReactElement => {
           id="songs"
           cols={30}
           rows={7}
+          value={trackData.songs}
+          onChange={onChangeData}
         ></textarea>
       </div>
 
